bf06b93ec0eaa5bd01a05099455a44e2fb27b2e6
Fernando Espino <fespino@grupoasercom.com>
2017-11-14 16:28:48 -0600

    Ajustes JSON

--- Inicio.aspx.cs
+++ Inicio.aspx.cs
@@ -3,21 +3,11 @@ using System.Web.Services;
 
 public partial class Inicio : System.Web.UI.Page
 {
-    //PageLoad aspx
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        DateTime FechaActual = DateTime.Now;
-        FechaActual = FechaActual.ToUniversalTime();
-        caja.InnerHtml = "Fecha Actual | " + Convert.ToString(FechaActual.ToShortDateString());
-    }
 
     //Manda llamar al Conector del WebService Diverza
     [WebMethod]
-    public static string LoadWeb(string pUsuario, string pContrasena)
+    public static string LoadWeb(string data)
     {
-        string result = Conector.Run();
-        //caja.InnerHtml = result;
-        return result;
-       
+        return Conector.Connect(data); 
     }
 }
